00113c2d49a0aa0250cff2de614ca47deed1a5e3
xxp2277 <60702545+xxp2277@users.noreply.github.com>
2020-02-06 15:21:32 +0800

    lean-sfe-fix-pbr.patch
    解决mwan3和fastpath冲突

--- diy.sh
+++ diy.sh
@@ -7,3 +7,4 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+wget -O- https://github.com/coolsnowwolf/lede/commit/e6ef0d1d73a4b3d4feaae55c86d68474700526d1.patch | patch -p1
