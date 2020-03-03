9421e200614531361bab8f9010daa03e6b8652b5
xxp2277 <60702545+xxp2277@users.noreply.github.com>
2020-03-03 20:38:59 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,6 +8,8 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-cd ~/lede/package/lean
+cd package/lean
 git clone https://github.com/pymumu/smartdns.git
 svn checkout https://github.com/Lienol/openwrt/trunk/package/lean/luci-app-smartdns
+./scripts/feeds update -a
+./scripts/feeds install -a
