5f7898e89ebbf761f2328aa5216265cbd6590b4d
xxp2277 <60702545+xxp2277@users.noreply.github.com>
2020-03-18 18:40:35 +0800

    切换回4.9内核

--- diy.sh
+++ diy.sh
@@ -11,7 +11,7 @@
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 sed -i '/^config internal themes/ a\        option Bootstrap "/luci-static/bootstrap"\n        option Netgear "/luci-static/netgear"' feeds/luci/modules/luci-base/root/etc/config/luci
-sed -i 's/KERNEL_PATCHVER:=4.9/KERNEL_PATCHVER:=4.14/g' target/linux/ar71xx/Makefile
+#sed -i 's/KERNEL_PATCHVER:=4.9/KERNEL_PATCHVER:=4.14/g' target/linux/ar71xx/Makefile
 cd package/lean
 git clone https://github.com/pymumu/smartdns.git
 svn checkout https://github.com/Lienol/openwrt/trunk/package/lean/luci-app-smartdns
