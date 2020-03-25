8183ab29653278227595b342074d35c66333770c
xxp2277 <60702545+xxp2277@users.noreply.github.com>
2020-03-25 22:26:06 +0800

    切换回4.14

--- diy.sh
+++ diy.sh
@@ -10,9 +10,10 @@
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
-wget -O- https://github.com/coolsnowwolf/lede/commit/e6ef0d1d73a4b3d4feaae55c86d68474700526d1.patch | patch -p1
+#解决sfe和mwan3的冲突补丁
+#wget -O- https://github.com/coolsnowwolf/lede/commit/e6ef0d1d73a4b3d4feaae55c86d68474700526d1.patch | patch -p1
 sed -i '/^config internal themes/ a\        option Bootstrap "/luci-static/bootstrap"\n        option Netgear "/luci-static/netgear"' feeds/luci/modules/luci-base/root/etc/config/luci
-#sed -i 's/KERNEL_PATCHVER:=4.9/KERNEL_PATCHVER:=4.14/g' target/linux/ar71xx/Makefile
+sed -i 's/KERNEL_PATCHVER:=4.9/KERNEL_PATCHVER:=4.14/g' target/linux/ar71xx/Makefile
 cd package/lean
 git clone https://github.com/pymumu/smartdns.git
 svn checkout https://github.com/Lienol/openwrt/trunk/package/lean/luci-app-smartdns
